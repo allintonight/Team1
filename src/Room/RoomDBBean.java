0fcfa26a5224d61b9e188728c28625493db6cb98
Suwon Baek Welamson <69312031+WonWonGit@users.noreply.github.com>
2020-09-21 23:05:43 +0900

    Update RoomDBBean.java
    DB 연결 ip로 연결 변경 !
    관리자 페이지에서 룸관리 부분 수정 
    룸 생성까지 완료

--- src/Room/RoomDBBean.java
+++ src/Room/RoomDBBean.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 public class RoomDBBean {
@@ -17,9 +18,9 @@ public class RoomDBBean {
 	
 	public Connection getConnection() throws Exception{
 		Connection con = null;
-		String url = "jdbc:mysql://localhost:3306/team1?serverTimezone=UTC"; 
-		String user = "root";
-		String pwd = "1234";
+		String url = "jdbc:mysql://203.245.44.74:3306/allintonight?serverTimezone=UTC"; 
+		String user = "allintonight";
+		String pwd = "team1team1";
 		
 		try { 
 			Class.forName("com.mysql.cj.jdbc.Driver");
@@ -30,6 +31,35 @@ public class RoomDBBean {
 			}
 		return con;
 	}
+	
+	public int addRoom(RoomBean roombean)  throws Exception{
+		Connection con=null;
+		PreparedStatement pstmt = null;
+		String sql=null;
+		int re=-1;
+		
+		try {
+			sql="insert into room values(?,?,?,?,?,?)";
+			con=getConnection();
+			pstmt=con.prepareStatement(sql);
+			pstmt.setInt(1, roombean.getRno());
+			pstmt.setString(2, roombean.getRname());
+			pstmt.setInt(3, roombean.getMen());
+			pstmt.setInt(4, roombean.getWeekday());
+			pstmt.setInt(5, roombean.getWeekend());
+			pstmt.setInt(6, roombean.getSweekday());
+			pstmt.executeUpdate();
+			re = 1;
+		}catch(Exception e) {
+			e.printStackTrace();
+			re = -1;
+		}finally {
+			con.close();
+			pstmt.close();
+		}
+		return re;
+	}
+	
 	
 	public ArrayList<RoomBean> getAll() throws SQLException {
 		Connection con=null;
@@ -56,5 +86,44 @@ public class RoomDBBean {
 		
 		return roomBean;
 		
+	}
+	@SuppressWarnings("null")
+	public ResultSet selectRoom(int rno) throws SQLException {
+		Connection con=null;
+		Statement stmt = null;
+		String sql=null;
+		ResultSet rs = null;
+		RoomBean roombean=null;
+		
+		try {
+			sql="select * from room where rno="+rno;
+			con=getConnection();
+			stmt=con.createStatement();
+			rs = stmt.executeQuery(sql);
+		}catch(Exception e) {
+			e.printStackTrace();
+			System.out.println(rno);
+		}
+		return rs;
+	}
+	
+	public int deleteRoom(int rno) {
+		Connection con=null;
+		PreparedStatement pstmt = null;
+		String sql=null;
+		int re=-1;
+		
+		try {
+			sql="delete from room where rno=?";
+			con=getConnection();
+			pstmt=con.prepareStatement(sql);
+			pstmt.setInt(1, rno);
+			pstmt.executeUpdate();
+			re = 1;
+		}catch(Exception e) {
+			e.printStackTrace();
+			re = -1;
+		}
+		return re;
 	}
 }
